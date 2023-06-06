a19891ab87406d7b6e9addc307d981f407f0cbdf
asluma <ase.mattson@gmail.com>
2023-06-06 20:19:36 +0200

    don't even know if I made any changes

--- commands/makepoll/makepoll.js
+++ commands/makepoll/makepoll.js
@@ -1,25 +1,33 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, Message} = require('discord.js');
 
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('makepoll')
-        .setDescription('Make a new poll of available dates for gaming - the one that works')
+        .setDescription('Make a new poll of available dates for gaming')
         .addStringOption(option =>
             option.setName('game')
                 .setDescription('Name of game')),
     async execute(interaction) {
         const game = interaction.options.getString('game')
-        await interaction.reply({content: `Here's hoping this outputs what you typed: ${game}`, ephemeral: false});
+
+        await interaction.reply({ content: `Eg vil spille ${game}`, fetchReply:true, ephemeral: true } )
+
+        await interaction.followUp('Her kjem ein liten (foreløpig) automatisk oppfølginsrespons.')
+        // input available dates
 
         const pollWindow = new EmbedBuilder()
             .setDescription(`Når kan du spille ${game}?`)
-            .setURL('https://discord.js.ord')
+            // .setURL('https://discord.js.org')
             .setDescription('This should probably be an explanation of how to vote? Idk.')
-            .addFields({ name: 'This is where the poll options should live.', value: 'Some more stuff' })
-            .setFooter({ text: 'Some profound text goes here. Or not.'});
+            .addFields({ name: '1.', value: 'Måndag er best, ingen protest.' })
+            .setFooter({ text: 'Gå ut å ta på gress.'});
 
-        interaction.channel.send( { embeds: [pollWindow]} )
+        interaction.channel.send( { embeds: [pollWindow], ephemeral:true} )
+
+
     },
+
+
 
 };
