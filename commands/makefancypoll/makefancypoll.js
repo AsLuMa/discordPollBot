204a5b4fccbd934bb8cd74eaaddd9f7664f0248d
asluma <ase.mattson@gmail.com>
2023-07-13 15:06:24 +0200

    some horribly implemented button logic

--- commands/makefancypoll/makefancypoll.js
+++ commands/makefancypoll/makefancypoll.js
@@ -6,6 +6,14 @@ import {
     EmbedBuilder, hideLinkEmbed, Message,
     SlashCommandBuilder
 } from 'discord.js';
+import 'node:timers/promises';
+
+export function makeButton(buttonId, name, buttonStyle) {
+    return new ButtonBuilder()
+        .setCustomId(buttonId)
+        .setLabel(name)
+        .setStyle(buttonStyle);
+}
 
 export const fancyPollCommand = {
     data: new SlashCommandBuilder()
@@ -18,63 +26,42 @@ export const fancyPollCommand = {
     async execute(interaction){
         // await interaction.reply({ content: 'This will surely be the fanciest poll ' + "\u2728", ephemeral: true}  )
 
-        function makeButton(buttonId, name, buttonStyle) {
-            return new ButtonBuilder()
-                .setCustomId(buttonId)
-                .setLabel(name)
-                .setStyle(buttonStyle);
-        }
-
-        const editButton = makeButton('editButton', 'Edit poll', ButtonStyle.Danger);
         const makePollButton = makeButton('makePoll', 'Start making poll!', ButtonStyle.Primary);
-        const doneEditingButton = makeButton('doneEditing', 'Done atoning for my sins', ButtonStyle.Primary);
-
-        // let buttons = [editButton, makePollButton];
 
         const firstMessageRow = new ActionRowBuilder();
         firstMessageRow.addComponents(makePollButton);
 
-        interaction.reply('At some point there will be a fancy description of how to make the poll here');
+        // todo deferReply()
+        await interaction.reply( {content: 'At some point there will be a fancy description of how to make the poll here'} );
 
-        interaction.channel.send( { components: [firstMessageRow], ephemeral: true } );
+        interaction.channel.send( { components: [firstMessageRow] } );
 
-        //make and send this on click
 
-        const pollWindow = new MessageEmbed()
-            .setDescription(`Todo: 
-            - add click listener to make poll button \n
-            - this starts a loop of asking user for input/dates \n
-            - make embed with the actual poll, and send it \n
-            - make logic for editing poll. Availabilities will change`);
-
-        const embedEditRow = new ActionRowBuilder();
-        embedEditRow.addComponents(editButton);
-
-        interaction.channel.send( { embeds: [pollWindow], ephemeral:true } );
-        interaction.channel.send( { components: [embedEditRow], ephemeral:true } );
-
-        const doneEditingRow = new ActionRowBuilder();
-        doneEditingRow.addComponents(doneEditingButton);
-
-        const editPlaceholder = new MessageEmbed()
-            .setDescription("This will at some point be an edit function. Probably.")
-
-        interaction.channel.send( { embeds: [editPlaceholder], ephemeral:true } );
-        interaction.channel.send( { components: [doneEditingRow], ephemeral:true } );
+        // const pollWindow = new MessageEmbed()
+        //     .setDescription(`Todo:
+        // - add click listener to make poll button \n
+        // - this starts a loop of asking user for input/dates \n
+        // - make embed with the actual poll, and send it \n
+        // - make logic for editing poll. Availabilities will change`);
+        //
+        // const embedEditRow = new ActionRowBuilder();
+        // embedEditRow.addComponents(editButton);
+        //
+        //
+        // interaction.channel.send( { embeds: [pollWindow], ephemeral:true }, makePollButton );
+        // interaction.channel.send( { components: [embedEditRow], ephemeral:true }, makePollButton );
 
 
 
-    //
-    //     const collectorFilter = i => i.user.id === interaction.user.id;
-    //
-    //     try {
-    //         const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60000 });
-    //         if (confirmation.customId === 'confirm') {
-    //             await confirmation.update({ content: `A very important update after clicking the button.`, components: [] });
-    //         }
-    //     } catch (e) {
-    //         await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
-    //     }
+        //
+        // const doneEditingRow = new ActionRowBuilder();
+        // doneEditingRow.addComponents(doneEditingButton);
+        //
+        // const editPlaceholder = new MessageEmbed()
+        //     .setDescription("This will at some point be an edit function. Probably.")
+        //
+        // interaction.channel.send({ embeds: [editPlaceholder], ephemeral:true });
+        // interaction.channel.send({ components: [doneEditingRow], ephemeral:true });
 
     },
 
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,9 @@
-import { Events } from 'discord.js';
+import {ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder as MessageEmbed, Events} from 'discord.js';
+import { makeButton } from "../commands/makefancypoll/makefancypoll.js";
+
+const editButton = makeButton('edit', 'Edit poll', ButtonStyle.Danger);
+const doneEditingButton = makeButton('doneEditing', 'Done atoning for my sins', ButtonStyle.Primary);
+
 
 export const interactionCreateEvent = {
     name: Events.InteractionCreate,
@@ -19,20 +24,55 @@ export const interactionCreateEvent = {
             }
 
         }
-        // else if (interaction.isButton()) {
-            // if (interaction.customId === 'editButton'){
-                // const clickEvent = interaction.client.click.get(interaction.onclick);
-                //
-                // try {
-                //     await clickEvent.execute(interaction);
-                // } catch (error) {
-                //     console.error(`Error executing command`);
-                //     console.error(error);
-                // }
+        else if (interaction.isButton()) {
 
-            // }
-            //button logic goes here
-        // }
+            if (interaction.customId === 'makePoll') {
+                console.log("clicked makepollbutton");
+
+                const pollWindow = new MessageEmbed()
+                    .setDescription(`Todo:
+                - fix the click listeners (can we move the logic in interactionCreate to makefancypoll.js?) \n
+                - this embed window should starts a loop where user is asked to input dates \n
+                - then the embed with the actual poll should be made and sent/rendered \n
+                - fix \'interaction failed\'-message`);
+
+                const embedEditRow = new ActionRowBuilder();
+                embedEditRow.addComponents(editButton);
+
+
+                interaction.channel.send( { embeds: [pollWindow], ephemeral:true } );
+                interaction.channel.send( { components: [embedEditRow], ephemeral:true } );
+
+
+            }
+
+            if (interaction.customId === 'edit'){
+                console.log("clicked editbutton");
+
+                const doneEditingRow = new ActionRowBuilder();
+                doneEditingRow.addComponents(doneEditingButton);
+
+                const editPlaceholder = new MessageEmbed()
+                    .setDescription("This will at some point be an edit function. Probably.")
+
+                interaction.channel.send({ embeds: [editPlaceholder], ephemeral:true });
+                interaction.channel.send({ components: [doneEditingRow], ephemeral:true });
+
+                }
+
+            if (interaction.customId === 'doneEditing') {
+                console.log("clicked atone button");
+                // interaction.reply({ content: "All done", ephemeral: true });
+                interaction.channel.send("This should return an updated version the embed window with the poll. " +
+                    "And an edit button, for further edits. " +
+                "We also have to deal with the emoji-reactions-somehow. Retain whatever has been voted on. Maybe change text color of the option that was changed.");
+
+            }
+
+
+        }
+
+
 
 
 
