1cf9f2ee81266abf30e7bf40e68cf8f28f4e989a
asluma <ase.mattson@gmail.com>
2023-07-13 12:16:31 +0200

    made basic flow of fancy poll

--- commands/makefancypoll/makefancypoll.js
+++ commands/makefancypoll/makefancypoll.js
@@ -1,41 +1,80 @@
-import {ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, SlashCommandBuilder} from 'discord.js';
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    EmbedBuilder as MessageEmbed,
+    EmbedBuilder, hideLinkEmbed, Message,
+    SlashCommandBuilder
+} from 'discord.js';
 
 export const fancyPollCommand = {
     data: new SlashCommandBuilder()
         .setName('fancypoll')
-        .setDescription('Fancy poll with buttons ' + "\u2728"),
+        .setDescription('Fancy poll with buttons ' + "\u2728")
+        .addStringOption(date1 =>
+            date1.setName('game')
+                .setDescription('What do you want to play?')),
 
     async execute(interaction){
-        await interaction.reply('This will surely be the fanciest poll ' + "\u2728" )
+        // await interaction.reply({ content: 'This will surely be the fanciest poll ' + "\u2728", ephemeral: true}  )
 
-        function makeButton(number) {
+        function makeButton(buttonId, name, buttonStyle) {
             return new ButtonBuilder()
-                .setCustomId(`Option ${number + 1}`)
-                .setLabel(`Alternativ ${number + 1}`)
-                .setStyle(ButtonStyle.Primary);
+                .setCustomId(buttonId)
+                .setLabel(name)
+                .setStyle(buttonStyle);
         }
 
-        let buttons = []
+        const editButton = makeButton('editButton', 'Edit poll', ButtonStyle.Danger);
+        const makePollButton = makeButton('makePoll', 'Start making poll!', ButtonStyle.Primary);
+        const doneEditingButton = makeButton('doneEditing', 'Done atoning for my sins', ButtonStyle.Primary);
 
-        for (let i = 0; i < 4; i++ ){
-            const button = makeButton(i)
-            buttons.push(button)
-        }
-// console.log(`buttons ${buttons}`)
+        // let buttons = [editButton, makePollButton];
 
-        const row = new ActionRowBuilder()
-        for (let i = 0; i < buttons.length; i++){
-            const button = buttons[i]
-            row.addComponents(button);
-        }
+        const firstMessageRow = new ActionRowBuilder();
+        firstMessageRow.addComponents(makePollButton);
 
-        // const pollWindow = new EmbedBuilder()
-        //     .setDescription(`Some content goes here?`);
+        interaction.reply('At some point there will be a fancy description of how to make the poll here');
+
+        interaction.channel.send( { components: [firstMessageRow], ephemeral: true } );
+
+        //make and send this on click
+
+        const pollWindow = new MessageEmbed()
+            .setDescription(`Todo: 
+            - add click listener to make poll button \n
+            - this starts a loop of asking user for input/dates \n
+            - make embed with the actual poll, and send it \n
+            - make logic for editing poll. Availabilities will change`);
+
+        const embedEditRow = new ActionRowBuilder();
+        embedEditRow.addComponents(editButton);
+
+        interaction.channel.send( { embeds: [pollWindow], ephemeral:true } );
+        interaction.channel.send( { components: [embedEditRow], ephemeral:true } );
+
+        const doneEditingRow = new ActionRowBuilder();
+        doneEditingRow.addComponents(doneEditingButton);
+
+        const editPlaceholder = new MessageEmbed()
+            .setDescription("This will at some point be an edit function. Probably.")
+
+        interaction.channel.send( { embeds: [editPlaceholder], ephemeral:true } );
+        interaction.channel.send( { components: [doneEditingRow], ephemeral:true } );
 
 
-    // embed with buttons
-    // interaction.channel.send( { embeds: [pollWindow], ephemeral:true, components: [row]} );
-    interaction.channel.send( { ephemeral:true, components: [row]} );
+
+    //
+    //     const collectorFilter = i => i.user.id === interaction.user.id;
+    //
+    //     try {
+    //         const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60000 });
+    //         if (confirmation.customId === 'confirm') {
+    //             await confirmation.update({ content: `A very important update after clicking the button.`, components: [] });
+    //         }
+    //     } catch (e) {
+    //         await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+    //     }
 
     },
 
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,24 +3,36 @@ import { Events } from 'discord.js';
 export const interactionCreateEvent = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName);
 
-        const command = interaction.client.commands.get(interaction.commandName);
+            if(!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
 
-        // this will interfere with a button handler
-        if(!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(`Error executing command`);
+                console.error(error);
+            }
+
         }
+        // else if (interaction.isButton()) {
+            // if (interaction.customId === 'editButton'){
+                // const clickEvent = interaction.client.click.get(interaction.onclick);
+                //
+                // try {
+                //     await clickEvent.execute(interaction);
+                // } catch (error) {
+                //     console.error(`Error executing command`);
+                //     console.error(error);
+                // }
 
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(`Error executing command`);
-            console.error(error);
-        }
-
-        // button handler goes here
+            // }
+            //button logic goes here
+        // }
 
 
 
